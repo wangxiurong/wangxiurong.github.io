c82dcb507e214355f41c469fc566734331b27125
王秀荣 <15369309067@163.com>
2020-06-04 13:51:23 +0800

    vue项目+elementUI项目搭建

---
+++ src/view/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <el-button @click="show = !show">Click Me</el-button>
+  
+     <el-radio v-model="number" label="1">备选项</el-radio>
+    <el-radio v-model="number" label="2">备选项</el-radio>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      show: true,
+      number:'1',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
